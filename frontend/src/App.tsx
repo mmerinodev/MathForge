54c0d0683c948a62e930e7016297d201f954d9b9
mmerino <mariomerinobaenammb@icloud.com>
2026-01-12 23:11:13 +0100

    frontend ok

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center text-3xl font-bold text-blue-600">
+      MathForge Frontend OK
+    </div>
+  )
+}
+
+export default App
